c28b900a76222adfc5392d45c313eba398a099f2
alynielson <nielsonaly@gmail.com>
2018-08-20 12:24:30 -0500

    get class average without lowest grade

---
+++ PracticeLINQ/ClassGradeAverage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLINQ
+{
+    class ClassGradeAverage
+    {
+
+        public double GetClassAverage(List<string> stringsOfGrades)
+        {
+            var classAverage = stringsOfGrades.Select(stringGrades => Array.ConvertAll<string, double>(stringGrades.Split(','), Convert.ToDouble).OrderBy(grade => grade).Skip(1).Average()).Average();
+            return classAverage;
+        } 
+    }
+}
